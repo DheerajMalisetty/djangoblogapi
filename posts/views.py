ddffc7e1bed4312ca573354d81628bc86c8464c3
dheerajmalisetty <malisettydheeraj@gmail.com>
2018-08-29 22:18:17 +0530

    essentials

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
@@ -12,9 +12,9 @@ def post_detail(request):
     return render(request, "index.html", context)
 
 def post_list(request):
-
+    queryset = Post.objects.all()
     context = {
-
+        "object_list": queryset,
         "title": "List"
     }
     return render(request, "index.html", context)
